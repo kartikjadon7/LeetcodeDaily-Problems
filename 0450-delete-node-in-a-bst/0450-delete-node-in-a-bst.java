0dc3e805f464f0e61ff44bbe79014c61777c058f
Kartik <kartikjadon7@gmail.com>
2026-07-20 12:35:43 +0530

    Time: 0 ms (100%), Space: 47.4 MB (77.98%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -1,57 +1,29 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
- 
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-      
-        if(root == null ) {
-            return null;
-        }
-        if(root.val<key){
-            root.right = deleteNode(root.right,key);
-        }
-        else if(root.val>key) {
-            root.left = deleteNode(root.left,key);
-        }
-        else{
-            
-            if(root.left == null && root.right == null ) {
-                return null;
-            }
-           
-            if(root.left ==  null){
-                return root.right;
-            }
-            else if(root.right ==  null ) {
-                return root.left;
-            }
+        if (root == null) return null;
 
-            TreeNode IS = findInOrderSuccessor(root.right);
-            root.val = IS.val;
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } 
+        else if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } 
+        else {
+            if (root.left == null) return root.right;
+            if (root.right == null) return root.left;
 
-            
-            root.right = deleteNode(root.right,IS.val);
+            TreeNode min = findMin(root.right);
+            root.val = min.val;
+            root.right = deleteNode(root.right, min.val);
         }
+
         return root;
-
     }
-    private TreeNode findInOrderSuccessor(TreeNode root){
-        while(root.left != null){
-            root = root.left;
+
+    private TreeNode findMin(TreeNode node) {
+        while (node.left != null) {
+            node = node.left;
         }
-        return root;
+        return node;
     }
 }
